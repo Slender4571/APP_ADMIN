a16030d89588e3223e41cce20cf0c6fdb4a37bef
Slender <felipesousa795@gmail.com>
2022-08-03 22:34:06 -0300

    Tabela v 0.3
    Tabela Funcionando

--- react/AdminCombos.tsx
+++ react/AdminCombos.tsx
@@ -2,143 +2,109 @@ import React, { FC, useEffect, useState, } from 'react'
 import { Layout, PageBlock, Table } from 'vtex.styleguide'
 import axios from 'axios'
 
+// interface User {
+//     name: String
+//     email: String
+//     prospectDate: String
+//     phone: String
+//     situation: String
+//     clientDate: String
+// }
+
+interface Combo {
+    quantidade: number
+    itens: string[]
+    id: string
+    comboProdutoId: string
+    productOneId: number
+    productOneName: string
+    productTwoId: number
+    productTwoName: string
+}
+
+// interface Response{
+//     data:Combo[]
+// }
+
 const defaultSchema = {
     properties: {
-        name: {
-            title: 'Name',
-            width: 250,
-            sortable: true,
+        "prodOneId": {
+            title: "Produto 1 Id",
+            width: 100,
         },
-        email: {
-            title: 'Email',
-            minWidth: 250,
+        "prodOneName": {
+            title: "Produto 1 Nome",
+            width: 250
         },
-        phone: {
-            title: 'Telefone',
-            minWidth: 100,
+        "prodTwoId": {
+            title: "Produto 2 Id",
+            width: 100
         },
-        situation: {
-            title: 'Situação',
-            minWidth: 100,
+        "prodTwoName": {
+            title: "Produto 2 Nome",
+            width: 250
         },
-        prospectDate: {
-            title: 'Prospecto desde',
-            minWidth: 200,
-        },
-        clientDate: {
-            title: 'Cliente desde',
-            minWidth: 200,
-        },
+        "quantidade": {
+            title: "Quantidade",
+            width: 100
+        }
     },
 }
 
 const AdminCombo: FC = () => {
 
-    let listCombos = [
-        {
-            name: 'Felipe',
-            email: 'name2',
-            prospectDate: 'name1',
-            phone: 'name1',
-            situation: 'name1',
-            clientDate: 'name1'
-        },
-        {
-            name: 'Leonardo',
-            email: 'name4',
-            prospectDate: 'name1',
-            phone: 'name1',
-            situation: 'name1',
-            clientDate: 'name1'
-        },
-        {
-            name: 'Wily',
-            email: 'name1',
-            prospectDate: 'name1',
-            phone: 'name1',
-            situation: 'name1',
-            clientDate: 'name1'
-        },
-        {
-            name: 'Marcel',
-            email: 'name1',
-            prospectDate: 'name1',
-            phone: 'name1',
-            situation: 'name1',
-            clientDate: 'name1'
-        },
-        {
-            name: 'Fernanda',
-            email: 'name5',
-            prospectDate: 'name1',
-            phone: 'name1',
-            situation: 'name1',
-            clientDate: 'name1'
-        },
-        {
-            name: 'Fernando',
-            email: 'name1',
-            prospectDate: 'name1',
-            phone: 'name1',
-            situation: 'name1',
-            clientDate: 'name1'
-        },
-    ]
-
     const [loading, setLoading] = useState(true);
-    const [data, setData] = useState<User[]>(listCombos);
-    const [sortedBy, setSortedtBy] = useState<String>("name");
-    const [sortedOrder, setSortedOrder] = useState<String>("ASC");
+    const [combos, setCombos] = useState<Combo[]>([]);
+    // const [sortedBy, setSortedtBy] = useState<String>("name");
+    // const [sortedOrder, setSortedOrder] = useState<String>("ASC");
 
     // const getData = async () => {
     //     const response = await axios.get('http://ec2-15-229-66-74.sa-east-1.compute.amazonaws.com:3000/combos') as Combo[]
     //     console.log(response)
     //     setLoading(false)
     //     return response
-
     // }
 
     useEffect(() => {
         const getData = async () => {
             try {
-                const response = await axios.get('http://ec2-15-229-66-74.sa-east-1.compute.amazonaws.com:3000/combos') as Combo[]
+                // const { data } = await axios.get('https://d6v3yemkcmgnnjhluk5ouoqloa0wwucm.lambda-url.us-east-1.on.aws/')
+                const { data } = await axios.get('https://combos-api.herokuapp.com/combos')
+                setCombos(data)
+                console.log(data);
                 setLoading(false)
-                console.log(response)
             } catch (error) { alert("Deu algo errado"); }
         };
         getData();
     }, []);
 
-    // useEffect(() => getData(), [])
+    // function handleSort() {
+    //     if (sortedBy === 'name') {
+    //         let novaLista =
+    // //             sortedOrder === "ASC"
+    // //                 ? data.sort((a,b)=> b.name.charCodeAt() - a.name.charCodeAt())
+    // //                 : data.sort((a,b)=> a.name.charCodeAt() - b.name.charCodeAt())
 
-    function handleSort() {
+    //         sortedOrder === "ASC"
+    //                 ? data!.sort(sortNameAlphapeticallyASC)
+    //                 : data!.sort(sortNameAlphapeticallyDESC)   
 
-        if (sortedBy === 'name') {
-            let novaLista =
-                sortedOrder === "ASC"
-                    ? data!.sort(sortNameAlphapeticallyASC)
-                    : data!.sort(sortNameAlphapeticallyDESC)
+    //         setSortedtBy("name")
+    //         sortedOrder === "ASC"?setSortedOrder("DESC"):setSortedOrder("ASC")
 
-            setSortedtBy("name")
-            if (sortedOrder === "ASC") {
-                setSortedOrder("DESC")
+    //         setData(combos)
+            
+    //         console.log("sortedOrder", sortedOrder)
+    //         console.log("sortedBy", sortedBy)
+    //     }
+    // }
 
-            } else { setSortedOrder("ASC") }
+    // function sortNameAlphapeticallyASC(user1: Combo, user2: Combo) {
+    //     return user1.name < user2.name ? -1 : user1.name > user2.name ? 1 : 0
+    // }
+    // function sortNameAlphapeticallyDESC(user1: Combo, user2: Combo) {
+    //     return user1.name < user2.name ? 1 : user1.name > user2.name ? -1 : 0
 
-            setData(novaLista)
-            console.log("data", data)
-            console.log("listCombos", novaLista)
-            console.log("sortedOrder", sortedOrder)
-            console.log("sortedBy", sortedBy)
-        }
-    }
-
-    function sortNameAlphapeticallyASC(user1: User, user2: User) {
-        return user1.name < user2.name ? -1 : user1.name > user2.name ? 1 : 0
-    }
-    function sortNameAlphapeticallyDESC(user1: User, user2: User) {
-        return user1.name < user2.name ? 1 : user1.name > user2.name ? -1 : 0
-    }
 
     // if (data.lengh <= 0) {
     //     return (
@@ -154,37 +120,22 @@ const AdminCombo: FC = () => {
 
     return (
         <Layout>
-            <PageBlock className="mb5">
+            <PageBlock title="Titulo" subtitle="Alguma explicação." variation="full" >
                 <Table
                     schema={defaultSchema}
-                    items={data}
+                    items={combos}
                     loading={loading}
                     fullWidt
-                    // sort={{
-                    //     sortedBy: sortedBy,
-                    //     sortedOrder: sortedOrder
-                    // }}
-                    onSort={handleSort}
+                    bg-danger
+                // sort={{
+                //     sortedBy: sortedBy,
+                //     sortedOrder: sortedOrder
+                // }}
+                // onSort={handleSort}
                 />
             </PageBlock>
         </Layout>
     )
 
-    interface User {
-        name: String
-        email: String
-        prospectDate: String
-        phone: String
-        situation: String
-        clientDate: String
-    }
-
-    interface Combo {
-        quantidade: number
-        itens: String[]
-        id: String
-        comboProdutoId: String
-        produtos: Object[]
-    }
 }
 export default AdminCombo
